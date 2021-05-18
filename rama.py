f862d027da6aa59dd40ff05a5387a39cbed0ae3f
saidou diallo <saidou224dev@hotmail.com>
2021-05-18 16:35:11 +0000

    lesson sur le maximum en python algorithmie

---
+++ rama.py
@@ -0,0 +1,10 @@
+n1=int(input("Donnez la valeur de n1"))
+n2=int(input("Donnez la valeur de n2"))
+n3=int(input("Donnez la valeur de n3"))
+if n1>n2:
+    max=n1
+else:
+    max=n2
+if n3>max:
+    max=n3
+print(" le Maximum est",max)        
