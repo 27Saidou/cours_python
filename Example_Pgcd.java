5c76524237ecff434119dadcb8b7812a8eb80ac3
saidou diallo <saidou224dev@hotmail.com>
2021-06-25 16:14:28 +0000

    lesson algorithmique sur le pgcd

--- Example_Pgcd.java
+++ Example_Pgcd.java
@@ -6,6 +6,7 @@ public class Example_Pgcd {
     public static void main(String[] args) {
         System.out.println(PGCD(15, 21));
     }
+    // cette methode permet de chercher le PGCD de deux nombres
     public static int PGCD(int a ,int b){
         if (a==b) {
             return a;
