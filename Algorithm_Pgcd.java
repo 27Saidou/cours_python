7d7aaf007267141d5d8084c1b2e7628971be8413
saidou diallo <saidou224dev@hotmail.com>
2021-06-30 15:01:20 +0000

    calcul du pgcd de deux nombre en java en utilisant la recursivité

---
+++ Algorithm_Pgcd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Algorithm_Pgcd {
+
+    public static int PGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return PGCD(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Veuillez entrez la valeur de A:");
+        int a = clavier.nextInt();
+        System.out.println("Veuillez entrez la valeur de B");
+        int b = clavier.nextInt();
+        System.out.println("Le PGCD de " + a + " et " + b + " est = " + PGCD(a, b));
+        clavier.close();
+    }
+}
