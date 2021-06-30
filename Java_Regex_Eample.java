880c091a1774a9256f3dc0e490e3f78041ec923c
saidou diallo <saidou224dev@hotmail.com>
2021-06-30 16:26:18 +0000

    lesson sur les expressions réguliers java regex

---
+++ Java_Regex_Eample.java
@@ -0,0 +1,10 @@
+import java.util.regex.*;
+
+public class Java_Regex_Eample {
+    public static void main(String[] args) {
+        String content = "c'est alpha diallo " + "from SinthiansLivre.com.";
+        String pattern = ".*Livre.*";
+        boolean isMatch=Pattern.matches(pattern,content);
+        System.out.println("Le text contient'livre'? " + isMatch);
+    }
+}
