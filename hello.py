02b63e1230e6eaf1663068ae7cbfcc8b58ff6ab0
saidou diallo <saidou224dev@hotmail.com>
2021-07-26 16:31:52 +0000

    create class fligth

---
+++ hello.py
@@ -0,0 +1,10 @@
+class Flight():
+    def __init__(self,passenger,name):
+        self.passenger = passenger,
+        self.name=[]
+        
+    def add_passenger(self,name):
+        self.name.append(name)
+        
+        
+
