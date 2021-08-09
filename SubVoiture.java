058a152d79ed3928737486bfe2440ba20f512507
saidou diallo <saidou224dev@hotmail.com>
2021-08-09 15:09:40 +0000

    Utilisation du mot cle super en java

--- SubVoiture.java
+++ SubVoiture.java
@@ -1,10 +1,18 @@
 public class SubVoiture  extends SuperMoto{
     int num=110;
+    void display(){
+    System.out.println("Methode de class Enfant");
+    }
     public void afficherNumber(){
         System.out.println(super.num);
+    }
+    public void afficherMessage(){
+        display();
+        super.display();
     }
     public static void main(String[] args) {
         SubVoiture obj= new SubVoiture();
         obj.afficherNumber();
+        obj.afficherMessage();
     }
 }
--- SuperMoto.java
+++ SuperMoto.java
@@ -1,3 +1,6 @@
 public class SuperMoto {
     int num=100;
+    void display(){
+        System.out.println("methode de la class Parent");
+    }
 }
