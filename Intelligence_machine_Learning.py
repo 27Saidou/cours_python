72bedf96df65347bc1c4b9418ea2928c59474416
saidou diallo <saidou224dev@hotmail.com>
2021-08-16 17:20:09 +0000

    astuce en python sur le positionnement des args

--- Intelligence_machine_Learning.py
+++ Intelligence_machine_Learning.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-activities=['Java','Python','C','C++']
-slices=[3,7,8,6]
+activities=['Java','Python','JavaScript','C#']
+slices = [16.29, 33.18, 7.25, 6.97]
 colors=['r','g','b','y']
 fig = plt.figure(figsize=(10, 7))
 plt.pie(slices,labels=activities,radius=1.2,
         startangle=90,colors=colors,
         shadow=True,autopct='%1.1f%%',
         explode=(0,0,0.1,0))
-plt.title('Analyse statistics des languages de programmation')
+plt.title('Classement des meilleurs langages de programmation 2021')
 plt.legend()
 plt.show()
 
