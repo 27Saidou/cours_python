412ccc97ab29aa6b3946c8632398a93a21bf32ac
saidou diallo <saidou224dev@hotmail.com>
2021-09-11 18:05:49 +0000

    type de formatage en python

---
+++ ExempleFormat.py
@@ -0,0 +1,15 @@
+name,age,sal="alpha diallo",27,1000.35
+# premier facon d'afficher les variables
+print(name,age,sal)
+# second format d'afficher les variables
+print("Name:%s Age:%dSal:%g" % (name,age,sal))
+# troisieme format d'afficher les variables
+print("Name est=",name)
+print("Age est ",age)
+print("Sal est",sal)
+# Quatrieme format d'afficher les variables
+print("Name{} Age:{} Salaire:{}".format(name,age,sal))
+# cinquieme format  d'afficher les variables
+print("Name{0} Age:{1} Salaire:{2}".format(name,age,sal))
+print(f"{name} {age} {sal}")
+print(f"Name={name} Age={age} salaire={sal}".format(name,age,sal))
