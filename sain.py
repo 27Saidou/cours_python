66f36a55554f4bd99bf26b2ad947e3c6b79896d1
saidou diallo <saidou224dev@hotmail.com>
2021-09-21 15:41:36 +0000

    lesson sur les fonctions en python

---
+++ sain.py
@@ -0,0 +1,25 @@
+def demander_nom():
+    reponse_nom=""""""
+    while reponse_nom=="":
+        reponse_nom=input("Quel est votre nom")
+    return reponse_nom
+
+
+def demander_age():
+    age_int=0
+    while age_int==0:
+        age_int=input("Donner votre age")
+    try:
+        age_int = int(age_int)
+    except:
+        print('Erreur. Vous devez rentrer un entier pour age')
+    return age_int
+
+# demander nom
+nom=demander_nom()
+
+# demander age
+age=demander_age()  
+    # afficher le resultat
+print("Tu t'appelle "+nom +",et Vous avez " + str(age)+"ans ")
+print("L'an prochain tu auras " +str(age+1)+" ans ")
