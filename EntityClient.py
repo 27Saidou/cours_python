7315a2070311d45d238af84c0f0031650f8ede44
saidou diallo <saidou224dev@hotmail.com>
2021-09-24 16:43:20 +0000

    lesson sur Heritage et encapsulation et polymorphisme

---
+++ EntityClient.py
@@ -0,0 +1,39 @@
+class Client():
+    compte=0
+    def __init__(self,numero,nom,prenom,tel,address):
+        self._numero = numero
+        self._nom = nom
+        self.prenom = prenom
+        self.tel = tel
+        self.address = address
+        Client.compte+=1
+
+
+    def __str__(self):
+        """cette methode permet de presenter le Client"""
+        
+        return "Numero:{0}\
+            \nNom:{1}\
+            \nPrenom:{2}\
+            \nTel:{3}\
+            \nAddress:{4}".format(self.numero,self.nom,self.prenom,self.tel,self.address) 
+
+    def _getNumero(self):
+        return self._numero
+
+    def _setNumero(self,newNumero):
+        self._numero = newNumero
+
+    numero=property(_getNumero,_setNumero)
+
+    def _getNom(self):
+        return self._nom 
+    def _setNom(self,newname):
+        self._nom = newname
+
+    nom=property(_getNom,_setNom)
+
+
+c2=Client(1,'Diallo','Alfa','621072627','Bailobaya')
+print('Vous avez creer',Client.compte,' un client')
+print(c2)
---
+++ EntityEtudiant.py
@@ -0,0 +1,37 @@
+from EntityClient import Client
+
+
+class Etudiant(Client):
+    def __init__(self, numero, nom, prenom, tel, address, filiere):
+        Client.__init__(self, numero, nom, prenom, tel, address)
+        self._filiere = filiere
+
+    def _getFiliere(self):
+        return self._filiere
+
+    def _setFiliere(self, newfiliere):
+        self._filiere = newfiliere
+
+    filiere = property(_getFiliere, _setFiliere)
+
+    def _getNom(self):
+        return self._nom
+
+    def _setNom(self, newname):
+        self._nom = newname
+
+    nom = property(_getNom, _setNom)
+
+    def __str__(self):
+        """cette methode permet de presenter l'etudiant"""
+        return "Numero:{0}\
+            \nNom:{1}\
+            \nPrenom:{2}\
+            \nTel:{3}\
+            \nAddress:{4}\
+            \nFiliere:{5}".format(self.numero, self.nom, self.prenom, self.tel, self.address, self.filiere)
+
+
+e1 = Etudiant(2, 'Diallo', 'Massoud', '77 123 34 56',
+            'Bailobaya', 'Engineer Java')
+print(isinstance(e1, Etudiant))
