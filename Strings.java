0e3f1a49892529bb7b80ad46126503ea940ec11e
saidou diallo <saidou224dev@hotmail.com>
2021-10-12 17:36:34 +0000

    je suis entrains de reviser sur java

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings{
+    public static void main(String[]args){
+        StringBuilder sb=new StringBuilder("B");
+        sb.append("o");
+        sb.append("n");
+        sb.append("j");
+        sb.append("o");
+        sb.append("u");
+        sb.append("r");
+        System.out.println(sb);
+    
+
+    }
+}
