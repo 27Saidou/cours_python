968fa5c3d3060a068f1dc615f70572a250256a98
saidou diallo <saidou224dev@hotmail.com>
2021-10-26 15:10:39 +0000

    Révision sur java

--- TableauxMulti.java
+++ TableauxMulti.java
@@ -1,5 +1,8 @@
 public class TableauxMulti {
     public static void main(String[] args) {
+        direBonjour("Diallo","alfa");
+
+
         int [][]scores={{ 5,3,6,15,3},{3,8,4},{18,14,2,9}};
         for (int i = 0; i < scores.length; i++) {
             for (int j = 0; j <scores[i].length;j++) {
@@ -7,5 +10,10 @@ public class TableauxMulti {
             }
             
         }
+        
+    }
+    
+    static void direBonjour(String firstName, String lastName) {
+        System.out.printf("Bonjour le developeur mobile %s %s\n", firstName, lastName);
     }
 }
