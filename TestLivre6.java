8d5f57ecd52c6c11a3925735b530bf3a85d4ccde
saidou diallo <saidou224dev@hotmail.com>
2021-10-30 14:58:53 +0000

    Revision sur les boolean en java

---
+++ TestLivre6.java
@@ -0,0 +1,12 @@
+public class TestLivre6 {
+    public static void main(String[] args) {
+        boolean condition=true;
+        if (condition) {
+            System.out.println("Condition est vrai");
+        } else {
+            System.out.println("Condition est false");
+        }
+        Boolean codOb=condition;
+        System.out.println(codOb.booleanValue());
+    }
+}
