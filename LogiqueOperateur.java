177202e7c9d7dc13c3b3cee459319eceb29b8e4a
saidou diallo <saidou224dev@hotmail.com>
2021-11-04 17:18:37 +0000

    Test Operateur Logique en java

---
+++ LogiqueOperateur.java
@@ -0,0 +1,12 @@
+public class LogiqueOperateur {
+    public static void main(String[] args) {
+        boolean beau = true;
+        boolean intelligent= true;
+        if (beau && intelligent) {
+            System.out.println("Vous etes beau et intelligent");
+        } else {
+            System.out.println("Dommage que vous etes un taulard");
+        }
+
+    }
+}
---
+++ OperateurLogique.java
@@ -0,0 +1,11 @@
+public class OperateurLogique{
+    public static void main(String[] args){
+        boolean beau= true;
+        boolean intelligent=false;
+        if(beau==true || intelligent==true){
+            System.out.println("Vous etes que ce soit beau ou intelligent");
+        }else{
+        System.out.println("Dommage!!!");
+        }
+    }
+}
