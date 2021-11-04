3e96443ae9d22e6759fbff766dcc04cb6f13c327
saidou diallo <saidou224dev@hotmail.com>
2021-11-04 16:28:41 +0000

    Application de gestion de produit

--- MonApplicationProduit.py
+++ MonApplicationProduit.py
@@ -1,32 +1,37 @@
 from Produit import *
 
+
 def creerFic(self):
-    fp=open('ListeProduit.txt','w')
-    n=int(input("Entrer le nombre de produit a mettre dans le fichier"))
-    for i in range(1,n+1):
-        p=Produit()
-        p.saisieProduit()
-    info=p.getCode()+"\t"+p.getNom()+"\t"+str(p.getPrix())+"\t"+str(p.getQte())+"\t"+p.getDateFabrication()+"\t"+p.getFournisseur()+"\n"
-    fp.write(info)
-    fp.close()
-
-def LireFic(self):
-    fp = open('ListeProduit.txt', 'r')
-    c=fp.read()
-    print("Le contenu du fichier est ")
-    print(c)
-    fp.close()
-
-def ajoutFic(self):
-    fp=open('ListeProduit.txt', 'a')
-    n=int(input("Entrer le nombre de produit a ajouter dans le fichier "))
-    for i in range(1,n+1):
-        p=Produit()
+    fp = open('ListeProduit.txt', 'w')
+    n = int(input("Entrer le nombre de produit a mettre dans le fichier"))
+    for i in range(1, n+1):
+        p = Produit()
         p.saisieProduit()
     info = p.getCode()+"\t"+p.getNom()+"\t"+str(p.getPrix())+"\t"+str(p.getQte()) + \
         "\t"+p.getDateFabrication()+"\t"+p.getFournisseur()+"\n"
     fp.write(info)
     fp.close()
+
+
+def LireFic(self):
+    fp = open('ListeProduit.txt', 'r')
+    c = fp.read()
+    print("Le contenu du fichier est ")
+    print(c)
+    fp.close()
+
+
+def ajoutFic(self):
+    fp = open('ListeProduit.txt', 'a')
+    n = int(input("Entrer le nombre de produit a ajouter dans le fichier "))
+    for i in range(1, n+1):
+        p = Produit()
+        p.saisieProduit()
+    info = p.getCode()+"\t"+p.getNom()+"\t"+str(p.getPrix())+"\t"+str(p.getQte()) + \
+        "\t"+p.getDateFabrication()+"\t"+p.getFournisseur()+"\n"
+    fp.write(info)
+    fp.close()
+
 
 creerFic()
 LireFic()
--- Produit.py
+++ Produit.py
@@ -45,18 +45,20 @@ class Produit():
 
     def getFournisseur(self):
         return self.fournisseur
+
     def saisieProduit(self):
-        self.code=input("Entrer le code du produit")
-        self.nom=input("Entrer le nom du produit")
-        self.prix=int(input("Entrer le prix du produit"))
-        self.qte=float(input("Entrer le quantite du produit"))
-        self.dateFabrication=input("Entrer la date de fabrication du  produit")
-        self.fournisseur=input("Entrer le fournisseur du produit")
-    
+        self.code = input("Entrer le code du produit")
+        self.nom = input("Entrer le nom du produit")
+        self.prix = int(input("Entrer le prix du produit"))
+        self.qte = float(input("Entrer le quantite du produit"))
+        self.dateFabrication = input(
+            "Entrer la date de fabrication du  produit")
+        self.fournisseur = input("Entrer le fournisseur du produit")
+
     def afficheProduit(self):
-        print("Le code est ",self.code)
-        print("Le nom du produit est ",self.nom)
-        print("Le prix unitaire  du produit est ",self.prix)
-        print("La quantite du produit est ",self.qte)
-        print("La date de fabrication est ",self.dateFabrication)
-        print("Le fournisseur du produit est ",self.fournisseur)
+        print("Le code est ", self.code)
+        print("Le nom du produit est ", self.nom)
+        print("Le prix unitaire  du produit est ", self.prix)
+        print("La quantite du produit est ", self.qte)
+        print("La date de fabrication est ", self.dateFabrication)
+        print("Le fournisseur du produit est ", self.fournisseur)
