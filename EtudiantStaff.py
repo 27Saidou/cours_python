a56de7c74578d1e8a16817c84529bdec11adc782
saidou diallo <saidou224dev@hotmail.com>
2021-11-08 15:46:34 +0000

    suite POO ENCAPSULATION

---
+++ EtudiantStaff.py
@@ -0,0 +1,60 @@
+class Student:
+    def __init__(self, name, age):
+        # private member
+        self.name = name
+        self.__age = age
+
+    # getter method
+    def get_age(self):
+        return self.__age
+
+    # setter method
+    def set_age(self, age):
+        self.__age = age
+
+
+stud = Student('Tima koure', 14)
+
+# retrieving age using getter
+print('Name:', stud.name, stud.get_age())
+
+# changing age using setter
+stud.set_age(16)
+
+# retrieving age using getter
+print('Name:', stud.name, stud.get_age())
+
+
+class Etudiant():
+    def __init__(self, name, roll_no, age):
+        # private member
+        self.name = name
+        # private members to restrict access
+        # avoid direct data modification
+        self.__roll_no = roll_no
+        self.__age = age
+
+    def show(self):
+        print('Details Etudiant :', self.name, self.__roll_no)
+
+    # getter methods
+    def get_roll_no(self):
+        return self.__roll_no
+
+    # setter method to modify data member
+    # condition to allow data modification with rules
+    def set_roll_no(self, number):
+        if number > 50:
+            print('Invalid roll no. Please set correct roll number')
+        else:
+            self.__roll_no = number
+
+
+jessa = Etudiant('Maimouna', 10, 15)
+
+# before Modify
+jessa.show()
+# changing roll number using setter
+jessa.set_roll_no(120)
+jessa.set_roll_no(25)
+jessa.show()
