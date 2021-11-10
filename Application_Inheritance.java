777432194315f407e060561dfdb949e8dfbc20b4
saidou diallo <saidou224dev@hotmail.com>
2021-11-10 16:26:07 +0000

    suite POO

---
+++ Application_Inheritance.java
@@ -0,0 +1,8 @@
+public class Application_Inheritance {
+    public static void main(String[] args) {
+        Inheritance_B ob = new Inheritance_B();
+        ob.c=30;
+        ob.affiche2();
+
+    }
+}
---
+++ Inheritance_B.java
@@ -0,0 +1,6 @@
+public class Inheritance_B extends Single_Inheritance {
+    int c;
+    void affiche2(){
+        System.out.println("La valeur de C est de :"+c);
+    }
+}
