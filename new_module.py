8e4bb65b194255a1b15f8b667f757a3044663fee
saidou diallo <saidou224dev@hotmail.com>
2021-11-11 16:54:58 +0000

    Gestion des modules en pthon

--- new_module.py
+++ new_module.py
@@ -1,7 +1,18 @@
 from mymodule import *
+from message import *
+import puissance
 print(person1["age"])
 fib(500)
 print(fib2(1000))
 print(addition(12,123))
 print(soustration(12,8))
 print(multiplication(12,2))
+print(hello("massoud"))
+print(ciao("woury"))
+print(bonjour("saidou developpeur mobile"))
+a=5
+u=puissance.carre(a)
+print("Le carre vaut", u)
+b=5
+v=puissance.cube(b)
+print("Le cube vaut ",v)
---
+++ puissance.py
@@ -0,0 +1,8 @@
+def carre(valeur):
+    resultat = valeur**2
+    return resultat
+
+
+def cube(valeur):
+    resultat = valeur**3
+    return resultat
