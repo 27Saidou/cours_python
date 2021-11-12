a13d7b01e3f14f9d2228d9fb7292c458ea8eea33
saidou diallo <saidou224dev@hotmail.com>
2021-11-12 15:53:06 +0000

    Gestion de nombre premier

--- Exo_Premier.java
+++ Exo_Premier.java
@@ -1,25 +1,27 @@
 import java.util.*;
+
 public class Exo_Premier {
     static int N, nd, d;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String message;
         System.out.println("Entrer la valeur de n:");
-        N=sc.nextInt();
-        nd=0;
-        for (d = 1;d<N; d++) {
-            if (N%d==0) {
-                nd+=1;
+        N = sc.nextInt();
+        nd = 0;
+        for (d = 1; d < N; d++) {
+            if (N % d == 0) {
+                nd += 1;
                 sc.close();
             }
         }
-        if (N==2) {
-            message="n est premier";
+        if (N == 2) {
+            message = "n est premier";
         } else {
-            message="n n'est pas premier";
-            
+            message = "n n'est pas premier";
+
         }
         System.out.println(message);
-        
+
     }
 }
