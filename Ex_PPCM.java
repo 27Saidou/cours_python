49637a30f02e2e5ef1aa7c9a7bc75eb618c511d8
saidou diallo <saidou224dev@hotmail.com>
2021-11-12 15:13:04 +0000

    TD algorithme de euclude PPCM

---
+++ Ex_PPCM.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Ex_PPCM {
+    static  int ppcm,a,b,y,x;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer la valeur de A:");
+        a = sc.nextInt();
+        System.out.println("Entrer la valeur de B:");
+        b = sc.nextInt();
+        x = a;
+        y = b;
+        while (x != y) {
+            if (x < y) {
+                x = x + a;
+            } else {
+                y = y + b;
+            }
+
+        }
+        ppcm=x; 
+        System.out.println("Le PPCM de (a et b)="+ppcm);
+        sc.close();
+
+    }
+}
