1c48049d85bfaae2d2982cb9ddc15e34d448647d
saidou diallo <saidou224dev@hotmail.com>
2021-11-12 16:38:22 +0000

    tdn nombre premier

--- Exo_Premier.java
+++ Exo_Premier.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
 public class Exo_Premier {
-    static int N, nd, d;
-
+    static int r,d;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String message;
-        System.out.println("Entrer la valeur de n:");
-        N = sc.nextInt();
-        nd = 0;
-        for (d = 1; d < N; d++) {
-            if (N % d == 0) {
-                nd += 1;
-                sc.close();
-            }
+        System.out.println("Veuillez entrer un nombre  entier :");
+        int n= sc.nextInt();
+        r=1;
+        d=2;
+        while(r!=0 && d<=n%2){
+            r=n%d;
+            d=d+1;
         }
-        if (N == 2) {
-            message = "n est premier";
+        if (r!=0) {
+        message="N est premier";
         } else {
-            message = "n n'est pas premier";
-
+        message="N n'est pas premier";
+    
         }
         System.out.println(message);
-
+        sc.close();
     }
 }
