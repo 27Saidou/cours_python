4a6d96030ef01c8e3317467d4daa7e3e3de27ec3
saidou diallo <saidou224dev@hotmail.com>
2021-11-15 15:58:12 +0000

    tri par selection en java et tri par insertion

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,4 @@
+
 public class Sorting {
     public static void printArray(int arr[]){
         for (int i = 0; i < arr.length; i++) {
@@ -5,17 +6,32 @@ public class Sorting {
         }
         System.out.println();
     }
+    // PGCD de deux nombres
+    public static int pgcd(int n1,int n2){
+            if (n1==n2) {
+                return n1;
+            }else{
+                if (n1>n2) 
+                    return pgcd(n1-n2,n2);
+                else
+                return pgcd(n1,n2-n1);
+            }
+        }
     public static void main(String[] args) {
         int arr[] = { 7, 8, 3, 1, 2 };
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[i] > arr[j + 1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j + 1];
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
         printArray(arr);
+        System.out.println(pgcd(60,15));
+
+        
+        
     }
 }
