ae8a2ca67435c749b2cd703f2d6fb74bc593826f
saidou diallo <saidou224dev@hotmail.com>
2021-11-15 17:01:54 +0000

    methode de gestion d'un factorielle en java

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Sorting {
     public static void printArray(int arr[]){
         for (int i = 0; i < arr.length; i++) {
@@ -17,6 +17,19 @@ public class Sorting {
                 return pgcd(n1,n2-n1);
             }
         }
+        public static void printFactoriel(int n){
+            if (n<0) {
+                System.out.println("Nombre Invalid");
+                return;
+            }
+            int factoriel =1;
+            for (int i = n; i >=1; i--) {
+                factoriel=factoriel*i;
+                
+            }
+            System.out.println(factoriel);
+            return;
+        }
     public static void main(String[] args) {
         int arr[] = { 7, 8, 3, 1, 2 };
         for (int i = 0; i < arr.length-1; i++) {
@@ -30,6 +43,12 @@ public class Sorting {
         }
         printArray(arr);
         System.out.println(pgcd(60,15));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Taper un nombre entier");
+        int n= sc.nextInt();
+        printFactoriel(n);
+        sc.close();
 
         
         
