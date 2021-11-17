031393909ea7a4e6391a36c1aeeeb981531580aa
saidou diallo <saidou224dev@hotmail.com>
2021-11-17 15:42:06 +0000

    suite algorithme sur fonction

--- Algorithme_Allocation_Familiale.java
+++ Algorithme_Allocation_Familiale.java
@@ -7,7 +7,7 @@ public class Algorithme_Allocation_Familiale {
         System.out.println("Entrer le nombre d'enfant ");
         int nombre_enfant = sc.nextInt();
         if (nombre_enfant <= 3) {
-            montant = nombre_enfant* 150;
+            montant = nombre_enfant * 150;
         } else if (nombre_enfant <= 6) {
             montant = 450 + (nombre_enfant - 3) * 38;
         } else {
@@ -15,13 +15,20 @@ public class Algorithme_Allocation_Familiale {
         }
         System.out.println("Le montant d'allocation familiale est:" + montant);
         sc.close();
-        
+
         // appelle de la fonction qui permet de calculer la somme de deux nbres
-        System.out.println(somme_de_deux_nombre(12,12));
+        System.out.println(somme_de_deux_nombre(12, 12));
+        int res=perimetre_rectangle(12, 8);
+        System.out.println(res);
     }
-    public static int somme_de_deux_nombre(int nombre1,int nombre2){
+
+    public static int somme_de_deux_nombre(int nombre1, int nombre2) {
         int resulat;
-        resulat=nombre1+nombre2;
+        resulat = nombre1 + nombre2;
         return resulat;
+    }
+
+    public static int perimetre_rectangle(int longueur, int largeur) {
+        return (2 * (longueur + largeur));
     }
 }
