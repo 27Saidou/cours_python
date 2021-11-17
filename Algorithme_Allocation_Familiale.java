931da79717ce84a7a93ffb64da69b39510904821
saidou diallo <saidou224dev@hotmail.com>
2021-11-17 15:33:38 +0000

    suite algorithme sur fonction

---
+++ Algorithme_Allocation_Familiale.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Algorithme_Allocation_Familiale {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int montant;
+        System.out.println("Entrer le nombre d'enfant ");
+        int nombre_enfant = sc.nextInt();
+        if (nombre_enfant <= 3) {
+            montant = nombre_enfant* 150;
+        } else if (nombre_enfant <= 6) {
+            montant = 450 + (nombre_enfant - 3) * 38;
+        } else {
+            montant = 654;
+        }
+        System.out.println("Le montant d'allocation familiale est:" + montant);
+        sc.close();
+        
+        // appelle de la fonction qui permet de calculer la somme de deux nbres
+        System.out.println(somme_de_deux_nombre(12,12));
+    }
+    public static int somme_de_deux_nombre(int nombre1,int nombre2){
+        int resulat;
+        resulat=nombre1+nombre2;
+        return resulat;
+    }
+}
