8c495b0053f569885c6f349b3f4a084ab94bc693
saidou diallo <saidou224dev@hotmail.com>
2021-11-22 18:18:37 +0000

    utilisation du mot cle this en java

--- ThisExample.java
+++ ThisExample.java
@@ -27,7 +27,61 @@ class ThisExample {
     public static void main(String[] args) {
         ThisExample obj=new ThisExample(1,-2);
         System.out.println(obj.toString());
+        Teste object = new Teste(10, 20);
+        object.display();
 
+        // creation d'un acteur
+        Actor ad1 = new Actor("Diallo","Alfa");
+        ad1.afficher_info_acteur();
+
+        Alfa t1 = new Alfa();
+        t1.display();
+
+    }
+}
+
+class Teste {
+    int a;
+    int b;
+
+    // Parameterized constructor
+    Teste(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    void display()
+    {
+        //afficher tout les variables
+        System.out.println("a = " + a + "  b = " + b);
+    }
+}
+
+class Actor {
+    String lastName;
+    String firstName;
+
+    public Actor(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+    public void afficher_info_acteur(){
+        System.out.println("lastName est :"+this.lastName);
+        System.out.println("firstName est:"+this.firstName);
+    }
+}
+
+class Alfa {
+
+    void display() {
+        // calling function show()
+        this.show();
+
+        System.out.println("Engineer java");
+    }
+
+    void show() {
+        System.out.println("Engineer Python");
     }
 }
 
