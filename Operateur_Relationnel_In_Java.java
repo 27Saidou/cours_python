91531bbea507cd3cb0d81293dedeaf55deb9e78d
saidou diallo <saidou224dev@hotmail.com>
2021-11-23 16:08:51 +0000

    type operateur en java

---
+++ Operateur_Relationnel_In_Java.java
@@ -0,0 +1,27 @@
+public class Operateur_Relationnel_In_Java {
+    public static void main(String[] args) {
+        // create variables
+        int a = 7, b = 11;
+
+        // value of a and b
+        System.out.println("a is " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b); // false
+
+        
+        System.out.println(a != b); // true
+
+        // > operator
+        System.out.println(a > b); // false
+
+        // < operator
+        System.out.println(a < b); // true
+
+        // >= operator
+        System.out.println(a >= b); // false
+
+        // <= operator
+        System.out.println(a <= b); // true
+    }
+}
---
+++ Type_Donnee_in_Java.java
@@ -0,0 +1,37 @@
+public class Type_Donnee_in_Java {
+    public static void main(String[] args) {
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);
+        int range = -4250000;
+        System.out.println(range);
+        long range2 = -42332200000L;
+        System.out.println(range2);
+        double number = -42.3;
+        System.out.println(number);
+        float number1 = -42.3f;
+        System.out.println(number1);
+        char letter2 = 65;
+        System.out.println(letter2);
+        String myString = "Programmation Java";
+        System.out.println(myString);
+
+        // declare variables
+        int a = 12, b = 5;
+
+        // addition operator
+        System.out.println("a + b = " + (a + b));
+
+        // subtraction operator
+        System.out.println("a - b = " + (a - b));
+
+        // multiplication operator
+        System.out.println("a * b = " + (a * b));
+
+        // division operator
+        System.out.println("a / b = " + (a / b));
+
+        // modulo operator
+        System.out.println("a % b = " + (a % b));
+    }
+}
