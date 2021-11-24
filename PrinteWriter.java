05c9cb33e5659458695b3e853cd8e6d5d8ac4ba9
saidou diallo <saidou224dev@hotmail.com>
2021-11-24 18:13:15 +0000

    Gestion Exception avec try-with-resource

--- PrinteWriter.java
+++ PrinteWriter.java
@@ -8,7 +8,7 @@ public class PrinteWriter {
             System.out.println("Entrer le try block");
             br = new BufferedReader(new FileReader("test.txt"));
             while ((line = br.readLine()) != null) {
-                System.out.println("Line =>" + line);
+                System.out.println("Ligne =>" + line);
             }
         } catch (IOException e) {
             System.out.println("IOException en try block =>" + e.getMessage());
