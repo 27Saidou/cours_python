4b756f79f705b05e61f9a3a42e092ecb6371178b
saidou diallo <saidou224dev@hotmail.com>
2021-11-24 17:59:52 +0000

    exception en java suite

---
+++ ListOfNumbers.java
@@ -0,0 +1,42 @@
+import java.io.*;
+public class ListOfNumbers {
+    // create list entier
+    private int[] list = { 5, 6, 8, 9, 2 };
+
+    public void writeList() {
+        PrintWriter out = null;
+
+        try {
+            System.out.println("Entering try statement");
+
+            // creation new file OutputFile.txt
+            out = new PrintWriter(new FileWriter("OutputFile.txt"));
+
+            //
+            for (int i = 0; i < 7; i++) {
+                out.println("Value at: " + i + " = " + list[i]);
+            }
+        }
+
+        catch (Exception e) {
+            System.out.println("Exception => " + e.getMessage());
+        }
+
+        finally {
+            if (out != null) {
+                System.out.println("Fermeture du printer");
+                // close PrintWriter
+                out.close();
+            }
+
+            else {
+                System.out.println("PrintWriter pas ouvert");
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        ListOfNumbers list=new ListOfNumbers();
+        list.writeList();
+    }
+}
