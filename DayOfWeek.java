81598b6486e5087c5531b5f685da56736a693808
saidou diallo <saidou224dev@hotmail.com>
2021-11-24 18:41:31 +0000

    fin du cours sur les assertions en java

---
+++ DayOfWeek.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer un jour svp");
+        String dayOfWeek =sc.nextLine();
+        switch (dayOfWeek) {
+        case "Sunday":
+            System.out.println("Dimanche");
+            break;
+        case "Monday":
+            System.out.println("Lundi");
+            break;
+        case "Tuesday":
+            System.out.println("Mardi");
+            break;
+        case "Wednesday":
+            System.out.println("Mercredi");
+            break;
+        case "Thursday":
+            System.out.println("jeudi");
+            break;
+        case "Friday":
+            System.out.println("Vendredi");
+            break;
+        case "Saturday":
+            System.out.println("Nous sommes Samedis");
+            break;
+            default:
+            System.out.println("Choisir un jour dans le weekends");
+            break;
+        }
+        sc.close();
+    }
+}
--- ExampleAssertion.java
+++ ExampleAssertion.java
@@ -1,4 +1,6 @@
 public class ExampleAssertion {
+    // Les assertions en Java aident à détecter les bogues en testant le code que
+    // nous supposons vrai
     public static void main(String[] args) {
         String[]weekends= { "Friday", "Saturday", "Sunday" };
         assert weekends.length==2;
