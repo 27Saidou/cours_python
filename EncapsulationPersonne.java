796175d53981afbeb52571595f1645d7fc374f98
saidou diallo <saidou224dev@hotmail.com>
2021-11-24 15:49:40 +0000

    fin du cours sur Encapsulation  en java

---
+++ EncapsulationPersonne.java
@@ -0,0 +1,7 @@
+public class EncapsulationPersonne {
+    public static void main(String[] args) {
+        ExamplePersonne p=new ExamplePersonne();
+        p.setAge(24);
+        System.out.println("J'ai " + p.getAge() +" ans ");
+    }
+}
