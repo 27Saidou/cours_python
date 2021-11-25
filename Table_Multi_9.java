16497209e373cf6ea5fb542d01f328650716ab04
saidou diallo <saidou224dev@hotmail.com>
2021-11-25 13:15:49 +0000

    les td en algorithmie

---
+++ Table_Multi_9.java
@@ -0,0 +1,12 @@
+public class Table_Multi_9 {
+    public static void main(String[] args) {
+        int p;
+        int i=1;
+        // table de multiplication algorithmie
+        while (i<=10) {
+            p=i*9;
+            System.out.println(i+" * 9 = "+ p);
+            i=i+1;
+        }
+    }
+}
