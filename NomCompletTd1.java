bed9a970ef41f9b8e1730d56ede997529d5c2d4c
saidou diallo <saidou224dev@hotmail.com>
2021-11-30 13:55:05 +0000

    algorithmie td1

---
+++ NomCompletTd1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class NomCompletTd1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Donnez votre nom svp");
+        String  firstname=sc.nextLine();
+        System.out.println("Donner votre prenom");
+        String lastname=sc.nextLine();
+        String nom_Comp=firstname + lastname;
+        System.out.println("Votre nom complet est :" + nom_Comp);
+        
+        }
+}
