86e361749dc4fdee6c468677d8c480a7113c7b70
saidou diallo <saidou224dev@hotmail.com>
2021-12-03 18:37:38 +0000

    lesson pointeur in c++

---
+++ Example_Pointeur.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int var1;
+    char var2[10];
+
+    cout << "Address de var1 variable: ";
+    cout << &var1 << endl;
+
+    cout << "Address de var2 variable: ";
+    cout << &var2 << endl;
+
+    return 0;
+}
