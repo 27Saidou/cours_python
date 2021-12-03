395c986c4cb506c835daa0970c66046dc1d49eb6
saidou diallo <saidou224dev@hotmail.com>
2021-12-03 18:18:41 +0000

    lesson two c++ loops

---
+++ Loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; ++i)
+    {
+        cout << i    << "je suis un developpeur java \n "<< endl;
+    }
+    return 0;
+}
