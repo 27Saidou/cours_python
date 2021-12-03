57cbb8b66eec6496d8b09a5fd4e5a78e803f47ee
saidou diallo <saidou224dev@hotmail.com>
2021-12-03 18:04:14 +0000

    first lesson c++

---
+++ Hello_World.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+// declaration Variable Global
+int g=20;
+int main(){
+    // Declaration Variable Local
+    int g=10;
+    cout<<g<<endl;
+    return 0;
+}
