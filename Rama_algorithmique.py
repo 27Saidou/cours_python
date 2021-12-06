e279f636b84a0ff6e38ebc804fd0d793fd744a06
saidou diallo <saidou224dev@hotmail.com>
2021-12-06 15:30:26 +0000

    loops en python exemple

--- Rama_algorithmique.py
+++ Rama_algorithmique.py
@@ -1,20 +1,20 @@
-first_number=int(input("Entrez votre premier nombre"))
-operator=input("choisir un un operateur (*,+,-,%,/):")
-second_number=int(input("Entrez votre second_number"))
-if operator=="+":
-    result=first_number+second_number
+first_number = int(input("Entrez votre premier nombre"))
+operator = input("choisir un un operateur (*,+,-,%,/):")
+second_number = int(input("Entrez votre second_number"))
+if operator == "+":
+    result = first_number+second_number
     print(result)
-elif operator=="-":
-    result=first_number-second_number
+elif operator == "-":
+    result = first_number-second_number
     print(result)
-elif operator=="*":
-    result=first_number*second_number
+elif operator == "*":
+    result = first_number*second_number
     print(result)
-elif operator=="%":
-    result=first_number%second_number
+elif operator == "%":
+    result = first_number % second_number
     print(result)
-elif operator=="/":
-    result=first_number/second_number
+elif operator == "/":
+    result = first_number/second_number
     print(result)
 else:
     print("Invalid operateur veuillez saisir un operateur valide")
