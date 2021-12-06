eabb2f4a50c4443600224f7f824b325032e9fab1
saidou diallo <saidou224dev@hotmail.com>
2021-12-06 18:33:04 +0000

    suite java OOPS

---
+++ Tabs_Rama.java
@@ -0,0 +1,6 @@
+public class Tabs_Rama {
+    public static void main(String[] args) {
+        int[][] finalMarks = { { 97, 98, 95 }, { 95, 95, 98 } };
+        System.out.println(finalMarks[1][1]);
+    }
+}
