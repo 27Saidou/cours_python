6d5b3a074a2605f41417cdb312042af62b73f927
saidou diallo <saidou224dev@hotmail.com>
2021-12-06 14:54:15 +0000

    utlisation des operateur en python exemple

---
+++ Rama_algorithmique.py
@@ -0,0 +1,20 @@
+first_number=int(input("Entrez votre premier nombre"))
+operator=input("choisir un un operateur(*,+,-,%,/) svp")
+second_number=int(input("Entrez votre second_number"))
+if operator=="+":
+    result=first_number+second_number
+    print(result)
+elif operator=="-":
+    result=first_number-second_number
+    print(result)
+elif operator=="*":
+    result=first_number*second_number
+    print(result)
+elif operator=="%":
+    result=first_number%second_number
+    print(result)
+elif operator=="/":
+    result=first_number/second_number
+    print(result)
+else:
+    print("Invalid operateur veuillez saisir un operateur valide")
