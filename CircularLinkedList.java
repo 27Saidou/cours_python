f58e80e3fc6ad59659dcb1377ac75697ce629892
saidou diallo <saidou224dev@hotmail.com>
2021-12-07 18:26:19 +0000

    linkedList structure donnees en java

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,4 +1,5 @@
 class CircularLinkedList {  
+    //Node definition for circular linked list  
     public class Node{  
         int data;  
         Node next;  
@@ -6,22 +7,30 @@ class CircularLinkedList {
             this.data = data;  
         }  
     }    
-
+    //Initially head and tail pointers point to null  
     public Node head = null;  
     public Node tail = null;  
+    //add new node to the circular linked list   
     public void add(int data){  
+        //Create new node  
         Node newNode = new Node(data);  
-        if(head == null) {    
+        //check if list is empty  
+        if(head == null) {  
+             //head and tail point to same node if list is empty  
             head = newNode;  
             tail = newNode;  
             newNode.next = head;  
         }  
         else {  
+            //tail points to new node if list is not empty
             tail.next = newNode;  
+            //New node becomes new tail.  
             tail = newNode;  
+            //tail points back to head  
             tail.next = head;  
         }  
     }  
+    //Display the nodes in circular linked list 
     public void displayList() {  
         Node current = head;  
         if(head == null) {  
@@ -29,8 +38,8 @@ class CircularLinkedList {
         }  
         else {  
             System.out.println("Circular linked list nodes: ");  
-            do{  
-                //affichage donnee  
+             do{  
+                //Print each node of the linked list  
                 System.out.print(current.data + " ");  
                 current = current.next;  
             }while(current != head);  
