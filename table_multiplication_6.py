6439d1e249d18b50009f432acae174c7277ea32c
saidou diallo <saidou224dev@hotmail.com>
2021-12-08 17:18:59 +0000

    table de multiplication en python

---
+++ table_multiplication_6.py
@@ -0,0 +1,10 @@
+number = int(input("Entrez la table de multiplication que vous voulez"))
+for count in range(1, 11):
+    print(number, 'x', count, '=', number * count)
+
+print("==========================================")
+i = 1
+while i <= 10:
+    p = i*7
+    print(i,'x 7=',p)
+    i += 1
