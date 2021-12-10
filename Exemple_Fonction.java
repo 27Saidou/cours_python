2b673ea6f3388818b7702a3877a188a9c1ef115b
saidou diallo <saidou224dev@hotmail.com>
2021-12-10 15:30:15 +0000

    Exemple fonction en java

--- Exemple_Fonction.java
+++ Exemple_Fonction.java
@@ -8,12 +8,22 @@ public class Exemple_Fonction {
         }
         return y;
     }
+    
+    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Entrez une valeur entier svp");
         int a= sc.nextInt();
         multi(a);
         sc.close();
+
+        int n1 = 60, n2 = 36, pgcd = 0;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0)
+                pgcd = i;
+        }
+        System.out.printf("PGCD de %d et %d est: %d", n1, n2, pgcd);
 
     }
     
