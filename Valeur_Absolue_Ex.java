4c50ae163bff55849d9f6fcf38c6e5a16e8e2536
saidou diallo <saidou224dev@hotmail.com>
2021-12-10 18:20:13 +0000

    exemple de valeur absolue en algorithmie java

---
+++ Valeur_Absolue_Ex.java
@@ -0,0 +1,9 @@
+
+public class Valeur_Absolue_Ex {
+    public static void main(String[] args) {
+        int i = -6;
+        double d = -2.5;
+        System.out.println(Math.abs(i));
+        System.out.println(Math.abs(d));
+    }
+}
