20b53641df327f199c0e7b7b4da34113e2121729
saidou diallo <saidou224dev@hotmail.com>
2021-12-14 15:48:14 +0000

    programmation orienté objet en python suite

---
+++ oop1.py
@@ -0,0 +1,47 @@
+class SoftwareEngineering:
+    # class attributes
+    alias = "KeyBoard Magician"
+
+    def __init__(self, name, age, level, salary):
+        # instance attributes
+        self.name = name
+        self.age = age
+        self.level = level
+        self.salary = salary
+
+    # instance method
+    def code(self):
+        print(f"{self.name} is writting code....")
+
+    # instance method
+    def code_in_language(self, language):
+        print(f"{self.name} is writting code in {language}....")
+
+    # instance method
+    def information(self):
+        information = f"name={self.name},age={self.age},level={self.level},salary={self.salary}"
+        return information
+
+    # dunder method
+    def __str__(self):
+        information = f"name={self.name},age={self.age},level={self.level},salary={self.salary}"
+        return information
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def entry_salary(age):
+        if age < 25:
+            return 5000
+        if age < 30:
+            return 7000
+        return 9000
+
+
+# instance
+se1 = SoftwareEngineering("Massoud", 20, "Junior", 5000)
+se2 = SoftwareEngineering("Alpha", 25, "Senior", 7000)
+se1.code()
+se1.code_in_language("Java")
+se2.code_in_language("Python")
+print(SoftwareEngineering.entry_salary(27))
