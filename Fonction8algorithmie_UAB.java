943c8512f48f32b7338570d96c736e726cf456b8
saidou diallo <saidou224dev@hotmail.com>
2021-12-14 14:53:38 +0000

    TD algo

---
+++ Fonction8algorithmie_UAB.java
@@ -0,0 +1,9 @@
+public class Fonction8algorithmie_UAB {
+    public static void main(String[] args) {
+        int i=1;
+        for (i = 0; i <10; i++) {
+            int res=i*5;
+            System.out.println(i +"X"+"5 ="+ res);
+        }
+    }
+}
