ad8f43ce9a368612df7f707949ce10b1d9e6b1ed
saidou diallo <saidou224dev@hotmail.com>
2021-12-16 15:42:50 +0000

    OOPS en java surcharge de methode ex

---
+++ Addition001.java
@@ -0,0 +1,9 @@
+public class Addition001 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
---
+++ PolyTest.java
@@ -0,0 +1,6 @@
+public class PolyTest {
+    public static void main(String[] args) {
+        System.out.println(Addition001.add(5, 5));
+        System.out.println(Addition001.add(2, 4, 6));
+    }
+}
