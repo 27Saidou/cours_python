7fbb414466e439ad9efada1317d604e7d22a06d5
saidou diallo <saidou224dev@hotmail.com>
2021-12-18 14:59:46 +0000

    python par la pratique

--- 0012.py
+++ 0012.py
@@ -1,5 +1,13 @@
-def agenda(nom,prenom,tel):
-    return{'nom':nom, 'prenom':prenom, 'tel':tel}
+def agenda(nom, prenom, tel):
+    return{'nom': nom, 'prenom': prenom, 'tel': tel}
 
-res=agenda('Diallo','Massoud',771412725)
+
+res = agenda('Diallo', 'Massoud', 771412725)
 print(res)
+
+age = int(input("Entrez votre age svp"))
+if age <= 18:
+    print("Mineur")
+else:
+    print("Majeur")
+    print("Fin")
